fc23e68c96376edb77d9a5caad87b3ded15d25c8
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-03-09 17:20:46 -0500

    change dropdown button

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, Form, FormControl, Button, DropdownButton } from 'react-bootstrap'
+import { Navbar, Nav, Dropdown } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCogs } from '@fortawesome/free-solid-svg-icons'
 import './Header.scss'
@@ -33,7 +33,7 @@ const alwaysOptions = (
       <Link to="/" className="nav-link">Home</Link>
     </li>
     <li className="nav-item active">
-      <Link to="/products" className="nav-link">Products</Link>
+      <Link to="/movies" className="nav-link">Movies</Link>
     </li>
   </React.Fragment>
 )
@@ -50,26 +50,32 @@ class Header extends Component {
       <header className="main-header">
         { user && <span>Welcome, {user.email}</span>}
         <Navbar bg={bgColor} variant={navColor} expand="lg">
-          <Link to='/'><Navbar.Brand>React-Bootstrap</Navbar.Brand></Link>
+          <Link to='/'><Navbar.Brand>Movie-React</Navbar.Brand></Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               { alwaysOptions }
             </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+            {/* (<Form inline>
+               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+               <Button variant="outline-success">Search</Button>
+             </Form> */}
             <Nav className="ml-auto">
               { user ? authenticatedOptions : unauthenticatedOptions }
             </Nav>
-            <DropdownButton drop='left' title={<span><FontAwesomeIcon icon={faCogs} /></span>}>
-              <h6>navbar color</h6>
-              <hr />
-              <button className='btn btn-dark btn-circle btn-lg' onClick={() => this.props.setColor('dark', 'dark')}></button>
-              <button className='btn btn-primary btn-circle btn-lg' onClick={() => this.props.setColor('primary', 'dark')}></button>
-              <button className='btn btn-light btn-circle btn-lg' onClick={() => this.props.setColor('light', 'light')}></button>
-            </DropdownButton>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <span><FontAwesomeIcon icon={faCogs} /></span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <h6>navbar color</h6>
+                <hr />
+                <button className='btn btn-dark btn-circle btn-lg' onClick={() => this.props.setColor('dark', 'dark')}></button>
+                <button className='btn btn-primary btn-circle btn-lg' onClick={() => this.props.setColor('primary', 'dark')}></button>
+                <button className='btn btn-light btn-circle btn-lg' onClick={() => this.props.setColor('light', 'light')}></button>
+              </Dropdown.Menu>
+
+            </Dropdown>
           </Navbar.Collapse>
         </Navbar>
       </header>)
