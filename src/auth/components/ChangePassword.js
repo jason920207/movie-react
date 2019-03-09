1994d11201e510be3d6032f0ca506897aa940db9
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-03-08 21:17:08 -0500

    rebuild changepassword rebuild signup ,add style in auth

--- src/auth/components/ChangePassword.js
+++ src/auth/components/ChangePassword.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-
 import { changePassword } from '../api'
 import messages from '../messages'
 
@@ -37,29 +36,52 @@ class ChangePassword extends Component {
     const { oldPassword, newPassword } = this.state
 
     return (
-      <form className='auth-form' onSubmit={this.onChangePassword}>
-        <h3>Change Password</h3>
-
-        <label htmlFor="oldpw">Old Password</label>
-        <input
-          required
-          name="oldPassword"
-          value={oldPassword}
-          type="password"
-          placeholder="Old Password"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="newPassword">New Password</label>
-        <input
-          required
-          name="newPassword"
-          value={newPassword}
-          type="password"
-          placeholder="New Password"
-          onChange={this.handleChange}
-        />
-        <button type="submit">Change Password</button>
-      </form>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xl-10 col-lg-12 col-md-9">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-2">Change Your Password?</h1>
+                      </div>
+                      <form className='user' onSubmit={this.onChangePassword}>
+                        <div className="form-group">
+                          <input
+                            className="form-control form-control-user"
+                            required
+                            name="oldPassword"
+                            value={oldPassword}
+                            type="password"
+                            placeholder="Old Password"
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <input
+                            className="form-control form-control-user"
+                            required
+                            name="newPassword"
+                            value={newPassword}
+                            type="password"
+                            placeholder="New Password"
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <button type="submit" className='btn btn-primary btn-user btn-block'>Change Password</button>
+                      </form>
+                      <hr />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 
 import { signUp, signIn } from '../api'
 import messages from '../messages'
@@ -40,38 +40,73 @@ class SignUp extends Component {
     const { email, password, passwordConfirmation } = this.state
 
     return (
-      <form className='auth-form' onSubmit={this.onSignUp}>
-        <h3>Sign Up</h3>
 
-        <label htmlFor="email">Email</label>
-        <input
-          required
-          name="email"
-          value={email}
-          type="email"
-          placeholder="Email"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          required
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Password"
-          onChange={this.handleChange}
-        />
-        <label htmlFor="passwordConfirmation">Confirm Password</label>
-        <input
-          required
-          name="passwordConfirmation"
-          value={passwordConfirmation}
-          type="password"
-          placeholder="Confirm Password"
-          onChange={this.handleChange}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
+      <div className="container">
+        <div className="card o-hidden border-0 shadow-lg my-5">
+          <div className="card-body p-0">
+            <div className="row">
+              <div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
+              <div className="col-lg-7">
+                <div className="p-5">
+                  <div className="text-center">
+                    <h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
+                  </div>
+                  <form className="user" onSubmit={this.onSignUp}>
+                    <div className="form-group">
+                      <input
+                        required
+                        name="email"
+                        className="form-control form-control-user"
+                        value={email}
+                        type="email"
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="form-group row">
+                      <div className="col-sm-6 mb-3 mb-sm-0">
+                        <input
+                          required
+                          className="form-control form-control-user"
+                          name="password"
+                          value={password}
+                          type="password"
+                          placeholder="Password"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                      <div className="col-sm-6">
+                        <input
+                          required
+                          className="form-control form-control-user"
+                          name="passwordConfirmation"
+                          value={passwordConfirmation}
+                          type="password"
+                          placeholder="Confirm Password"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-user btn-block">Sign Up</button>
+                    <hr />
+                    <a href="index.html" className="btn google btn-user btn-block">
+                      <i className="fab fa-google fa-fw"></i> Register with Google
+                    </a>
+                    <a href="index.html" className="btn facebook btn-user btn-block">
+                      <i className="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                    </a>
+                  </form>
+                  <hr />
+                  <div className="text-center">
+                    <Link to='/sign-in' className="small">Already have an account? Login!</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     )
   }
 }
