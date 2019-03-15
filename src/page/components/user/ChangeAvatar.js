b836601730c19673304cf27d5b4120f895f05f6c
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-03-15 07:07:30 -0400

    fix image size

--- src/page/components/user/ChangeAvatar.js
+++ src/page/components/user/ChangeAvatar.js
@@ -71,7 +71,7 @@ class ChangeAvatar extends Component {
             )} />} >
             <Modal.Header>Play A Game?</Modal.Header>
             <Modal.Content image>
-              <Image as='a' href='https://jason920207.github.io/tic-tac-toe/' wrapped size='medium' src='https://s3.us-east-2.amazonaws.com/jasonbucket/2000px-Tic_tac_toe.svg.png' />
+              <Image as='a' href='https://jason920207.github.io/tic-tac-toe/' wrapped size='huge' src='https://s3.us-east-2.amazonaws.com/jasonbucket/2000px-Tic_tac_toe.svg.png' />
             </Modal.Content>
           </Modal>
           <Modal centered={false} trigger={ <Image as='a' src='https://s3.us-east-2.amazonaws.com/jasonbucket/10455766_657348364314222_7463937380643555250_n1.jpg' size='tiny'
