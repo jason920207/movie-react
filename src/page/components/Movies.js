63d8e40dd209ec209974e2f9e7209f4eadc29088
xiaojie zhang <xiaojiezhang@S0106441c120f48e6.ed.shawcable.net>
2019-03-19 11:42:21 -0400

    change page

--- src/page/components/Movies.js
+++ src/page/components/Movies.js
@@ -19,7 +19,7 @@ class Products extends Component {
       moviesByDate: null,
       totalPages: 10,
       activePage: 1,
-      moviesPerPage: 10
+      moviesPerPage: 20
     }
     this.onClickTopMovie = this.onClickTopMovie.bind(this)
     this.onClickMovieByDate = this.onClickMovieByDate.bind(this)
